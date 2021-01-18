e0f67888aad97bd18208dbbe5728e126f5d2c34d
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2021-01-18 20:14:56 +0100

    Add row actions to dataset table

--- src/webapp/src/pages/Datasets.jsx
+++ src/webapp/src/pages/Datasets.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import styled from 'styled-components';
 import MaterialTable from 'material-table';
 import { useTranslation } from 'react-i18next';
+
+import setClipboardText from '../utils/clipboard';
 
 const PAGE_SIZES = [5, 10, 15, 30, 50, 75, 100];
 
@@ -35,11 +37,25 @@ const COLUMNS = [
 
 function Datasets(props) {
   const { t } = useTranslation();
-  const { data } = props;
+  const { className, data } = props;
 
   const onReload = () => {
     // TODO: reload data
     return data;
+  };
+
+  const onDatasetDelete = (rowData) => {
+    // TODO: add delete logic
+    return rowData;
+  };
+
+  const onDatasetDownload = (rowData) => {
+    const a = document.createElement('a');
+    // TODO: use correct URL
+    a.href = `http://localhost:30002/api/projects/ml-lab-demo/files/download?fileKey=datasets%2Fnews-categorized.csv.v1`;
+    a.target = '_blank';
+    a.download = rowData.name || 'download';
+    a.click();
   };
 
   return (
@@ -73,17 +89,57 @@ function Datasets(props) {
           onClick: onReload,
           tooltip: t('reload'),
         },
+        {
+          icon: 'download',
+          iconProps: { className: `${className} actionIcon` },
+          onClick: (event, rowData) => {
+            onDatasetDownload(rowData);
+          },
+          tooltip: 'Download dataset',
+        },
+        {
+          icon: 'content_copy',
+          iconProps: { className: `${className} actionIcon` },
+          onClick: (event, rowData) => {
+            setClipboardText(rowData.name);
+          },
+          tooltip: 'Copy dataset key',
+        },
+        {
+          icon: 'delete',
+          iconProps: { className: `${className} actionIcon` },
+          onClick: (event, rowData) => {
+            onDatasetDelete(rowData);
+          },
+          tooltip: 'Delete dataset',
+        },
       ]}
     />
   );
 }
 
 Datasets.propTypes = {
+  className: PropTypes.string,
   data: PropTypes.arrayOf(Object),
 };
 
 Datasets.defaultProps = {
-  data: [],
+  className: '',
+  data: [
+    {
+      name: 'Foobar',
+      modifiedAt: 'a month ago',
+      modifiedBy: 'admin',
+      version: 2,
+      size: '8.32 mb',
+    },
+  ],
 };
 
-export default Datasets;
+const StyledDatasets = styled(Datasets)`
+  &.actionIcon {
+    color: rgba(0, 0, 0, 0.54);
+  }
+`;
+
+export default StyledDatasets;
