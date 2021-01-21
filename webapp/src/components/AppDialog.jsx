17656d14d62f559940f7c1de47f3579ad8982bd0
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2021-01-21 19:06:58 +0100

    Refactor AppDialog

--- webapp/src/components/AppDialog.jsx
+++ webapp/src/components/AppDialog.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 
-import Button from '@material-ui/core/Button';
-
-const AppDialog = React.memo(({ children, onClose }) => {
+const AppDialog = React.memo(({ children }) => {
   const domEl = document.getElementById('app-dialog');
 
   if (!domEl) return null;
 
   // This is where the magic happens -> our modal div will be rendered into our 'modal-root' div, no matter where we
   // use this component inside our React tree
-  return ReactDOM.createPortal(
-    <div>
-      <Button onClick={onClose}>Close</Button>
-      {children}
-    </div>,
-    domEl
-  );
+  return ReactDOM.createPortal(<div>{children}</div>, domEl);
 });
 
 AppDialog.propTypes = {
@@ -25,7 +17,6 @@ AppDialog.propTypes = {
     PropTypes.instanceOf(Object),
     PropTypes.arrayOf(Object),
   ]).isRequired,
-  onClose: PropTypes.func.isRequired,
 };
 
 export default AppDialog;
