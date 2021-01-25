f070b20eec18220aeb3cbbc3c265833faaaad20e
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2021-01-25 12:54:36 +0100

    Allow Iframe to be project specific

--- webapp/src/pages/Iframe.jsx
+++ webapp/src/pages/Iframe.jsx
@@ -3,20 +3,34 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import ReactIframe from 'react-iframe';
 
+import GlobalStateContainer from '../app/store';
+
 function Iframe(props) {
-  const { className, url } = props;
+  const { className, url, projectSpecific } = props;
+  const { activeProject } = GlobalStateContainer.useContainer();
+  let iframeUrl = url;
+  if (projectSpecific) {
+    iframeUrl = `${url}?project=${activeProject.id}`;
+  }
+
   return (
-    <ReactIframe url={url} allowFullScreen className={`${className} root`} />
+    <ReactIframe
+      url={iframeUrl}
+      allowFullScreen
+      className={`${className} root`}
+    />
   );
 }
 
 Iframe.propTypes = {
   className: PropTypes.string,
   url: PropTypes.string.isRequired,
+  projectSpecific: PropTypes.bool,
 };
 
 Iframe.defaultProps = {
   className: '',
+  projectSpecific: false,
 };
 
 const StyledIframeComponent = styled(Iframe)`
