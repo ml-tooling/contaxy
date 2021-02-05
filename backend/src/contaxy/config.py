a99576c8551d9f468714a8e9283358d06919d96e
Lukas Masuch <lukas.masuch@gmail.com>
2021-02-05 13:57:44 +0000

    Minor fixes

--- backend/src/contaxy/config.py
+++ backend/src/contaxy/config.py
@@ -6,4 +6,4 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     secret_key: str = "secret"
     acces_token_expiry_minutes: Optional[int] = 30
-    jwt_algorithm = "HS256"
+    jwt_algorithm: str = "HS256"
