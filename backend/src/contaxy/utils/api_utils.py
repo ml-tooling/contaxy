34ebe0f69b6d75fb894925578fbcd0fab2d71821
Lukas Masuch <lukas.masuch@gmail.com>
2021-02-05 18:50:04 +0000

    Add redoc relative URL patch

--- backend/src/contaxy/utils/api_utils.py
+++ backend/src/contaxy/utils/api_utils.py
@@ -7,9 +7,18 @@ def patch_fastapi(app: FastAPI) -> None:
     This patch is required to make fastapi fully functional with a relative url path.
     This code snippet can be copy-pasted to any Fastapi application.
     """
-    from fastapi.openapi.docs import get_swagger_ui_html
+    from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
     from starlette.requests import Request
     from starlette.responses import HTMLResponse
+
+    async def redoc_ui_html(req: Request) -> HTMLResponse:
+        assert app.openapi_url is not None
+        redoc_ui = get_redoc_html(
+            openapi_url="./" + app.openapi_url.lstrip("/"),
+            title=app.title + " - Redoc UI",
+        )
+
+        return HTMLResponse(redoc_ui.body.decode("utf-8"))
 
     async def swagger_ui_html(req: Request) -> HTMLResponse:
         assert app.openapi_url is not None
@@ -42,9 +51,14 @@ def patch_fastapi(app: FastAPI) -> None:
     for app_route in app.routes:
         if app_route.path == "/docs":  # type: ignore
             continue
+
+        if app_route.path == "/redoc":  # type: ignore
+            continue
+
         routes_new.append(app_route)
 
     app.router.routes = routes_new
 
     assert app.docs_url is not None
     app.add_route(app.docs_url, swagger_ui_html, include_in_schema=False)
+    app.add_route(app.redoc_url, redoc_ui_html, include_in_schema=False)
