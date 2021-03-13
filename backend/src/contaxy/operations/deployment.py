2d855ad5d55907d29987a615868b4604b0bebf23
Lukas Masuch <lukas.masuch@gmail.com>
2021-03-13 13:57:16 +0000

    Move stuff to migrate

--- backend/src/contaxy/operations/deployment.py
+++ backend/src/contaxy/operations/deployment.py
@@ -13,6 +13,14 @@ class ServiceOperations(ABC):
         self,
         project_id: str,
     ) -> List[Service]:
+        """Lists all services associated with the given project.
+
+        Args:
+            project_id (str): The project ID to filter the services.
+
+        Returns:
+            List[Service]: The list of services associated with the project.
+        """
         pass
 
     @abstractmethod
@@ -22,6 +30,23 @@ class ServiceOperations(ABC):
         service: ServiceInput,
         action_id: Optional[str] = None,
     ) -> Service:
+        """Deploys a service for the specified project.
+
+        If no `action_id` is provided, the system will automatically select the best deployment option.
+
+        Available deployment options (actions) can be requested via the [list_deploy_service_actions](#services/list_deploy_service_actions) operation.
+        If the action is from an extension, the `action_id` must be a composite ID with the following format: `{extension_id}~{action_id}`.
+
+        The action mechanism is further explained in the description of the [list_deploy_service_actions](#services/list_deploy_service_actions).
+
+        Args:
+            project_id (str): [description]
+            service (ServiceInput): [description]
+            action_id (Optional[str], optional): [description]. Defaults to None.
+
+        Returns:
+            Service: [description]
+        """
         pass
 
     @abstractmethod
@@ -29,7 +54,16 @@ class ServiceOperations(ABC):
         self,
         project_id: str,
         service: ServiceInput,
-    ) -> ResourceAction:
+    ) -> List[ResourceAction]:
+        """Lists all available service deployment options (actions).
+
+        Args:
+            project_id (str): The project ID associated with the service.
+            service_id (str): The ID of the service.
+
+        Returns:
+            List[ResourceAction]: Available deployment actions.
+        """
         pass
 
     @abstractmethod
@@ -38,6 +72,15 @@ class ServiceOperations(ABC):
         project_id: str,
         service_id: str,
     ) -> Service:
+        """Returns the metadata of a single service.
+
+        Args:
+            project_id (str): The project ID associated with the service.
+            service_id (str): The ID of the service.
+
+        Returns:
+            Service: The service metadata.
+        """
         pass
 
     @abstractmethod
@@ -47,6 +90,16 @@ class ServiceOperations(ABC):
         service_id: str,
         delete_volumes: bool = False,
     ) -> None:
+        """Deletes a service.
+
+        Args:
+            project_id (str): The project ID associated with the service.
+            service_id (str): The ID of the service.
+            delete_volumes (bool, optional): If `True`, all attached volumes will be deleted. Defaults to `False`.
+
+        Raises:
+            RuntimeError: If an error occurs during the deletion of the service.
+        """
         pass
 
     @abstractmethod
@@ -57,6 +110,21 @@ class ServiceOperations(ABC):
         lines: Optional[int],
         since: Optional[datetime],
     ) -> str:
+        """Returns the logs of a service.
+
+        Args:
+            project_id (str): The ID of the project into which the service is deployed in.
+            service_id (str): The ID of the service.
+            lines (Optional[int]): If provided, just the last `n` lines are returned from the log. Defaults to `None`.
+            since (Optional[datetime]): If provided, just the logs since the given timestamp are returned. Defaults to `None`.
+
+        Raises:
+            NotImplementedError: [description]
+            RuntimeError: If reading the logs of the given service fails.
+
+        Returns:
+            str: The logs of the service.
+        """
         pass
 
     @abstractmethod
@@ -65,6 +133,15 @@ class ServiceOperations(ABC):
         project_id: str,
         container_image: str,
     ) -> ServiceInput:
+        """[summary]
+
+        Args:
+            project_id (str): [description]
+            container_image (str): [description]
+
+        Returns:
+            ServiceInput: [description]
+        """
         pass
 
     @abstractmethod
@@ -74,6 +151,16 @@ class ServiceOperations(ABC):
         service_id: str,
         extension_id: Optional[str],
     ) -> ResourceAction:
+        """[summary]
+
+        Args:
+            project_id (str): [description]
+            service_id (str): [description]
+            extension_id (Optional[str]): [description]
+
+        Returns:
+            ResourceAction: [description]
+        """
         pass
 
     @abstractmethod
@@ -83,6 +170,16 @@ class ServiceOperations(ABC):
         service_id: str,
         action_id: str,
     ) -> Response:
+        """[summary]
+
+        Args:
+            project_id (str): [description]
+            service_id (str): [description]
+            action_id (str): [description]
+
+        Returns:
+            Response: [description]
+        """
         pass
 
     @abstractmethod
@@ -92,6 +189,16 @@ class ServiceOperations(ABC):
         service_id: str,
         endpoint: str,
     ) -> Response:
+        """[summary]
+
+        Args:
+            project_id (str): [description]
+            service_id (str): [description]
+            endpoint (str): [description]
+
+        Returns:
+            Response: [description]
+        """
         pass
 
 
