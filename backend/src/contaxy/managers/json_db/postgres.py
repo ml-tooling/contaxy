5700f5eb0e0b97b88cf9ae73592ee55b2350df80
Jan Kalkan <jan.kalkan@mailbox.org>
2021-03-16 18:34:13 +0000

    Refactor PostgresJsonDocumentManager

--- backend/src/contaxy/managers/json_db/postgres.py
+++ backend/src/contaxy/managers/json_db/postgres.py
@@ -1,16 +1,16 @@
 import datetime
 import json
-from typing import List, Optional
+from typing import Dict, List, Optional
 
-from sqlalchemy import Column, DateTime, Table, func, text
+from sqlalchemy import Column, DateTime, MetaData, Table, func, text
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.engine import Row
 from sqlalchemy.exc import NoResultFound, ProgrammingError
-from sqlalchemy.future import Engine
-from sqlalchemy.schema import DDL, CreateSchema
+from sqlalchemy.future import Engine, create_engine
 
 from contaxy.operations import JsonDocumentOperations
 from contaxy.schema.json_db import JsonDocument
+from contaxy.utils.postgres_utils import create_schema
 from contaxy.utils.state_utils import GlobalState, RequestState
 
 
@@ -28,21 +28,34 @@ class PostgresJsonDocumentManager(JsonDocumentOperations):
         """
         self.global_state = global_state
         self.request_state = request_state
+        self._engine = self._create_db_engine()
+        self._metadata = MetaData()
 
     def create_json_document(
         self,
         project_id: str,
         collection_id: str,
         key: str,
-        json_document: str,
+        json_document: Dict,
     ) -> JsonDocument:
-        """Upsert."""
+        """Creates a json document for a given key.
+
+        An upsert strategy is used, i.e. if a document already exists for the given key it will be overwritten. The project is equivalent to the DB schema and the collection to a DB table inside the respective DB schema. Schema as well as table will be lazily created.
+
+        Args:
+            project_id (str): Project Id, i.e. DB schema.
+            collection_id (str): Json document collection Id, i.e. DB table.
+            key (str): Json Document Id, i.e. DB row key.
+            json_document (Dict): The actual Json document.
+
+        Returns:
+            JsonDocument: The created Json document.
+        """
 
         table = self._get_collection_table(project_id, collection_id)
 
-        insert_data = self._add_metadata_for_insert(
-            json_document.dict(exclude_none=True)
-        )
+        insert_data = {"key": key, "json_value": json_document}
+        insert_data = self._add_metadata_for_insert(insert_data)
         upsert_data = self._add_metadata_for_update(insert_data)
 
         upsert_statement = (
@@ -54,14 +67,30 @@ class PostgresJsonDocumentManager(JsonDocumentOperations):
         with self._engine.begin() as conn:
             result = conn.execute(upsert_statement)
             if result.rowcount == 0:
-                raise Exception("Upsert failed")
+                raise RuntimeError("Upsert failed")
             conn.commit()
 
-        return self.get_document(project_id, collection_id, json_document.key)
+        return self.get_json_document(project_id, collection_id, key)
 
-    def get_document(
+    def get_json_document(
         self, project_id: str, collection_id: str, key: str
     ) -> JsonDocument:
+        """Get a Json document by key.
+
+        The project is equivalent to the DB schema and the collection to a DB table inside the respective DB schema. Schema as well as table will be lazily created.
+
+        Args:
+            project_id (str): Project Id, i.e. DB schema.
+            collection_id (str): Json document collection Id, i.e. DB table.
+            key (str): Json Document Id, i.e. DB row key.
+            json_document (Dict): The actual Json document.
+
+        Raises:
+            ValueError: No document found for the given key.
+
+        Returns:
+            JsonDocument: The requested Json document.
+        """
         table = self._get_collection_table(project_id, collection_id)
         select_statement = table.select().where(table.c.key == key)
         with self._engine.begin() as conn:
@@ -72,11 +101,22 @@ class PostgresJsonDocumentManager(JsonDocumentOperations):
                 raise ValueError(f"No document with key {key} found")
         return self._map_db_row_to_document_model(row)
 
-    def get_documents(
+    def get_json_documents(
         self, project_id: str, collection_id: str, keys: List[str]
     ) -> List[JsonDocument]:
-        if not keys:
-            raise ValueError("At least one key must be provided")
+        """Get multiple Json documents by key.
+
+        The project is equivalent to the DB schema and the collection to a DB table inside the respective DB schema. Schema as well as table will be lazily created.
+
+        Args:
+            project_id (str): Project Id, i.e. DB schema.
+            collection_id (str): Json document collection Id, i.e. DB table.
+            keys (List[str]): Json Document Ids, i.e. DB row keys.
+            json_document (Dict): The actual Json document.
+
+        Returns:
+            JsonDocument: The requested Json document.
+        """
 
         table = self._get_collection_table(project_id, collection_id)
         select_statement = table.select().where(table.c.key.in_(keys))
@@ -85,20 +125,38 @@ class PostgresJsonDocumentManager(JsonDocumentOperations):
             rows = result.fetchall()
         return self._map_db_rows_to_document_models(rows)
 
-    def update_document(
-        self, project_id: str, collection_id: str, document: JsonDocument
+    def update_json_document(
+        self,
+        project_id: str,
+        collection_id: str,
+        key: str,
+        json_document: dict,
     ) -> JsonDocument:
-        """Updates the document via Json Merge Patch strategy and returns the updated document."""
+        """Updates a Json document via Json Merge Patch strategy.
+
+        The project is equivalent to the DB schema and the collection to a DB table inside the respective DB schema. Schema as well as table will be lazily created.
+
+        Args:
+            project_id (str): Project Id, i.e. DB schema.
+            collection_id (str): Json document collection Id, i.e. DB table.
+            key (str): Json Document Id, i.e. DB row key.
+            json_document (Dict): The actual Json document.
+
+        Raises:
+            ValueError: No document found for the given key.
+
+        Returns:
+            JsonDocument: The updated document.
+        """
         table = self._get_collection_table(project_id, collection_id)
-        json_value = document.json_value
-        if isinstance(json_value, dict):
-            json_value = json.dumps(document.json_value)
+
+        json_value = json.dumps(json_document)
 
         update_data = self._add_metadata_for_update({})
 
         sql_statement = (
             table.update()
-            .where(table.c.key == document.key)
+            .where(table.c.key == key)
             .values(
                 json_value=func.jsonb_merge_patch(table.c.json_value, json_value),
                 **update_data,
@@ -106,19 +164,41 @@ class PostgresJsonDocumentManager(JsonDocumentOperations):
         )
 
         with self._engine.begin() as conn:
-            conn.execute(sql_statement)
+            result = conn.execute(sql_statement)
+            if result.rowcount == 0:
+                # This will raise a value error if doc not exists
+                self.get_json_document(project_id, collection_id, key)
+                raise RuntimeError(
+                    f"Document {key} could not be deleted (project_id: {project_id}, collection_id {collection_id})"
+                )
             conn.commit()
 
-        return self.get_document(project_id, collection_id, document.key)
+        return self.get_json_document(project_id, collection_id, key)
 
-    def delete_document(self, project_id: str, collection_id: str, key: str) -> None:
+    def delete_json_document(
+        self, project_id: str, collection_id: str, key: str
+    ) -> None:
+        """Delete a Json document by key.
+
+        The project is equivalent to the DB schema and the collection to a DB table inside the respective DB schema. Schema as well as table will be lazily created.
+
+        Args:
+            project_id (str): Project Id, i.e. DB schema.
+            collection_id (str): Json document collection Id, i.e. DB table.
+            key (str): Json Document Id, i.e. DB row key.
+            json_document (Dict): The actual Json document.
+
+        Raises:
+            ValueError: No document found for the given key.
+        """
         table = self._get_collection_table(project_id, collection_id)
         delete_statement = table.delete().where(table.c.key == key)
         with self._engine.begin() as conn:
             result = conn.execute(delete_statement)
-            if result.rowcount != 1:
-                # TODO: Use specific excp
-                raise Exception(
+            if result.rowcount == 0:
+                # This will raise a value error if doc not exists
+                self.get_json_document(project_id, collection_id, key)
+                raise RuntimeError(
                     f"Document {key} could not be deleted (project_id: {project_id}, collection_id {collection_id})"
                 )
             conn.commit()
@@ -126,33 +206,57 @@ class PostgresJsonDocumentManager(JsonDocumentOperations):
     def delete_documents(
         self, project_id: str, collection_id: str, keys: List[str]
     ) -> int:
+        """Delete Json documents by key.
+
+        The project is equivalent to the DB schema and the collection to a DB table inside the respective DB schema. Schema as well as table will be lazily created.
+
+        Args:
+            project_id (str): Project Id, i.e. DB schema.
+            collection_id (str): Json document collection Id, i.e. DB table.
+            keys (List[str]): Json Document Ids, i.e. DB row keys.
+            json_document (Dict): The actual Json document.
+
+        """
         table = self._get_collection_table(project_id, collection_id)
         delete_statement = table.delete().where(table.c.key.in_(keys))
         with self._engine.begin() as conn:
             result = conn.execute(delete_statement)
-            # TODO raise meaningful error
             conn.commit()
         return result.rowcount
 
-    def get_collection(
-        self, project_id: str, collection_id: str, filter: Optional[str] = None
+    def list_json_documents(
+        self,
+        project_id: str,
+        collection_id: str,
+        filter: Optional[str] = None,
     ) -> List[JsonDocument]:
-        # TODO: Evaluate JsonPath, incl indices
-        # table = self._get_collection_table(project_id, collection_id)
+        """List all existing Json documents and optionally filter via Json Path syntax.
 
-        # select_statement = table.select()
+            The project is equivalent to the DB schema and the collection to a DB table inside the respective DB schema. Schema as well as table will be lazily created.
 
-        # for filter_str in filters:
-        #     select_statement = select_statement.where(
-        #         table.c.json_value.contains(filter_str)
-        #     )
+        Args:
+            project_id (str): Project Id, i.e. DB schema.
+            collection_id (str): Json document collection Id, i.e. DB table.
+            filter (Optional[str], optional): Json Path filter. Defaults to None.
+
+        Raises:
+            ValueError: If filter is provided and does not contain a valid Json Path filter.
+
+        Returns:
+            List[JsonDocument]: List of Json documents.
+        """
+        # TODO: Check if SQL Injection needs to be prevented
+
         with self._engine.begin() as conn:
 
-            stmt = f'select * from "{collection_id}".{project_id}'
+            stmt = f'select * from "{project_id}".{collection_id}'
             if filter:
-                stmt = f"{stmt} {filter}"
+                stmt = f"{stmt} where json_value {filter}"
+            try:
+                result = conn.execute(text(stmt))
+            except ProgrammingError:
+                raise ValueError("Please provide a valid Json Path filter.s")
 
-            result = conn.execute(text(stmt))
             rows = result.fetchall()
         return self._map_db_rows_to_document_models(rows)
 
@@ -205,68 +309,17 @@ class PostgresJsonDocumentManager(JsonDocumentOperations):
         except ProgrammingError as err:
             if err.code != "f405":
                 raise err
-            self._create_schema(project_id)
+            create_schema(self._engine, project_id)
             collection.create(self._engine, checkfirst=True)
 
         return collection
 
-    def _create_schema(self, project_id: str) -> None:
-
-        with self._engine.begin() as conn:
-            stmt = CreateSchema(project_id)
-            try:
-                conn.execute(stmt)
-                conn.commit()
-                print(f"Postgres Schema {project_id} created")
-            except ProgrammingError:
-                # This should only happen if the schema exists already
-                pass
-
-
-def create_jsonb_merge_patch_func(engine: Engine) -> None:
-
-    func = DDL(
-        "create or replace function jsonb_merge_patch(v_basedoc jsonb, v_patch jsonb) "
-        "returns jsonb as $$ "
-        "with recursive patchexpand as("
-        "select '{}'::text[] as jpath, v_patch as jobj, jsonb_typeof(v_patch) as jtype, 0 as lvl "
-        "union all "
-        "select p.jpath||o.key as jpath, p.jobj->o.key as jobj, jsonb_typeof(p.jobj->o.key) as jtype, p.lvl + 1 as lvl "
-        "from patchexpand p "
-        "cross join lateral jsonb_each(case when p.jtype = 'object' then p.jobj else '{}'::jsonb end) as o(key, value) "
-        "), pathnum as ( "
-        "select *, row_number() over (order by lvl, jpath) as rn "
-        "from patchexpand "
-        "), apply as ("
-        "select case "
-        "when jsonb_typeof(v_basedoc) = 'object' then v_basedoc "
-        "else '{}'::jsonb "
-        "end as basedoc, "
-        "p.rn "
-        "from pathnum p "
-        "where p.rn = 1 "
-        "union all "
-        "select case "
-        "when p.jtype = 'object' then a.basedoc "
-        "when p.jtype = 'null' then a.basedoc #- p.jpath "
-        "else jsonb_set(a.basedoc, p.jpath, p.jobj) "
-        "end as basedoc, "
-        "p.rn "
-        "from apply a "
-        "join pathnum p "
-        "on p.rn = a.rn + 1 "
-        ") "
-        "select case "
-        "when jsonb_typeof(v_patch) != 'object' then v_patch "
-        "else basedoc "
-        "end "
-        "from apply "
-        "order by rn desc "
-        "limit 1; "
-        "$$ "
-        "language sql;"
-    )
-
-    with engine.begin() as conn:
-        conn.execute(func)
-        conn.commit()
+    def _create_db_engine(self) -> Engine:
+        state_namespace = self.request_state[PostgresJsonDocumentManager]
+        if not state_namespace.db_engine:
+            url = self.global_state.settings.POSTGRES_CONNECTION_URI
+            engine = create_engine(url, future=True)
+            # Test the DB connection and set to global state if succesful
+            with engine.begin():
+                state_namespace.db_engine = engine
+        return state_namespace.db_engine
