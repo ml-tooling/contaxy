20095810d7035cbd99e1c339f4702d6a0bbf2c21
Lukas Masuch <lukas.masuch@gmail.com>
2021-03-17 23:20:24 +0000

    Add documentation for auth operations

--- backend/src/contaxy/operations/auth.py
+++ backend/src/contaxy/operations/auth.py
@@ -29,6 +29,21 @@ class AuthOperations(ABC):
         token_type: TokenType = TokenType.SESSION_TOKEN,
         description: Optional[str] = None,
     ) -> str:
+        """Returns a session or API token with access to the speicfied scopes.
+
+        Args:
+            authorized_token: The authorized token used to create the new token.
+            scopes (optional): Scopes requested for this token. If none specified, the token will be generated with same set of scopes as the authorized token.
+            token_type (optional): The type of the token. Defaults to `TokenType.SESSION_TOKEN`.
+            description (optional): A short description about the generated token.
+
+        Raises:
+            PermissionDeniedError:
+            UnauthenticatedError: If the token is invalid or expired.
+
+        Returns:
+            str: The created session or API token.
+        """
         pass
 
     @abstractmethod
@@ -37,6 +52,21 @@ class AuthOperations(ABC):
         token: str,
         permission: Optional[str] = None,
     ) -> GrantedPermission:
+        """Verifies a session or API token.
+
+        The token is verfied for its validity and - if provided - if it has the specified permission.
+
+        Args:
+            token: Token to verify.
+            permission (optional): The token is checked if it is granted this permission. If none specified, only the existence or validity of the token itself is checked.
+
+        Raises:
+            PermissionDeniedError: If the requested permission is denied.
+            UnauthenticatedError: If the token is invalid or expired.
+
+        Returns:
+            GrantedPermission: Information about the granted permission and authenticated user.
+        """
         pass
 
     @abstractmethod
@@ -45,6 +75,14 @@ class AuthOperations(ABC):
         user_id: str,
         password: str,
     ) -> None:
+        """Changes the password of a given user.
+
+        The password is stored as a hash.
+
+        Args:
+            user_id: The ID of the user.
+            password: The new password to apply for the user.
+        """
         pass
 
     @abstractmethod
@@ -53,6 +91,17 @@ class AuthOperations(ABC):
         user_id: str,
         password: str,
     ) -> bool:
+        """Verifies a password of a specified user.
+
+        The password is stored as a hash
+
+        Args:
+            user_id: The ID of the user.
+            password: The password to check. This can also be specified as a hash.
+
+        Returns:
+            bool: `True` if the password matches the stored password.
+        """
         pass
 
     # Permission Operations
@@ -63,24 +112,55 @@ class AuthOperations(ABC):
         resource_name: str,
         permission: str,
     ) -> None:
+        """Grants a permission to the specified resource.
+
+        Args:
+            resource_name: The resource name that the permission is granted to.
+            permission: The permission to grant to the specified resource.
+        """
         pass
 
     @abstractmethod
     def remove_permission(
         self, resource_name: str, permission: str, apply_as_prefix: bool = False
     ) -> None:
+        """Revokes a permission from the specified resource.
+
+        Args:
+            resource_name: The resource name that the permission should be revoked from.
+            permission: The permission to revoke from the specified resource.
+            apply_as_prefix: If `True`, the permission is used as prefix, and all permissions that start with this prefix will be revoked. Defaults to `False`.
+        """
         pass
 
     @abstractmethod
     def list_permissions(
         self, resource_name: str, resolve_groups: bool = True
     ) -> List[str]:
+        """Returns all permissions associated with the specified resource.
+
+        Args:
+            resource_name: The name of the resource (relative URI).
+            resolve_groups: If `True`, all permission will be resolved to basic permissions. Defaults to `True`.
+
+        Returns:
+            List[str]: List of permissions associated with the given resource.
+        """
         pass
 
     @abstractmethod
     def list_resources_with_permissions(
         self, permission: str, resource_name_prefix: Optional[str] = None
     ) -> List[str]:
+        """Returns all resources that are granted for the specified permission.
+
+        Args:
+            permission: The permission to use. If the permission is specified without the access level, it will filter for all access levels.
+            resource_name_prefix: Only return resources that match with this prefix.
+
+        Returns:
+            List[str]: List of resources names (relative URIs).
+        """
         pass
 
 
