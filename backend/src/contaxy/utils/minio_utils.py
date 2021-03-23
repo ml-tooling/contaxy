63e67f7bd6dcd65d91a1e8d628d54250d18f41dd
Jan Kalkan <jan.kalkan@mailbox.org>
2021-03-23 13:57:02 +0100

    Add minio utils incl. tests

--- backend/src/contaxy/utils/minio_utils.py
+++ backend/src/contaxy/utils/minio_utils.py
@@ -1,23 +1,82 @@
-from typing import Optional
-
+from loguru import logger
 from minio import Minio
+from minio.error import S3Error
 from minio.versioningconfig import ENABLED, VersioningConfig
+from urllib3.exceptions import MaxRetryError
+
+from contaxy.config import Settings
+from contaxy.schema.exceptions import ServerBaseError
+
+
+def get_bucket_name(project_id: str, prefix: str) -> str:
+    return project_id if not prefix else f"{prefix}.{project_id}"
+
+
+def create_minio_client(settings: Settings) -> Minio:
+    client = Minio(
+        endpoint=settings.S3_ENDPOINT,
+        access_key=settings.S3_ACCESS_KEY,
+        secret_key=settings.S3_SECRET_KEY,
+        region=settings.S3_REGION,
+        secure=settings.S3_SECURE,
+    )
+
+    try:
+        if not client.bucket_exists("nonexistingbucket"):
+            logger.debug("Object storage connected")
+    except (MaxRetryError, S3Error):
+        logger.critical(
+            f"Could not connect to object storage (endpoint: {settings.S3_ENDPOINT}, region: {settings.S3_REGION}, secure: {settings.S3_SECURE})."
+        )
+        raise ServerBaseError(
+            "Could not connect to object storage. Check S3 endpoint configuration."
+        )
+    return client
+
+
+def delete_bucket(client: Minio, bucket_name: str) -> None:
+    pass
 
 
 def create_bucket(
     client: Minio,
     bucket_name: str,
-    region: Optional[str] = None,
     versioning_enabled: bool = True,
 ) -> None:
-    """Create a object storage bucket.
+    """Create a bucket in the object storage configured by the client.
 
     Args:
         client (Minio): The initilaized Minio client.
         bucket_name (str): Bucket name.
-        region (Optional[str], optional): The region. Defaults to None.
         versioning_enabled (bool, optional): Controls bucket versioning. Defaults to True.
+
+    Raises:
+        ServerBaseError: If the desired action could not be performed.
     """
-    client.make_bucket(bucket_name, region)
+    # TODO: Handle Errors
+    try:
+        client.make_bucket(bucket_name)
+        logger.debug(f"Bucket {bucket_name} created.")
+    except S3Error as err:
+        if err.code == "BucketAlreadyOwnedByYou":
+            logger.info(f"The bucket {bucket_name} already exists and is owned by you.")
+        else:
+            logger.error(
+                f"Could not create create the bucket {bucket_name} for an unknown reason."
+            )
+            raise ServerBaseError(f"Could not create create the bucket {bucket_name}.")
+
     if versioning_enabled:
-        client.set_bucket_versioning(bucket_name, VersioningConfig(ENABLED))
+        # TODO: Add handling if versioning could not be enabled
+        try:
+            client.set_bucket_versioning(bucket_name, VersioningConfig(ENABLED))
+        except S3Error as err:
+            msg = ""
+            if err.code == "NotImplemented":
+                msg = f"Versioning could not be enabled on the bucket {bucket_name}. Ensure that the object storage is configured with versioning enabled."
+            else:
+                msg = f"Versioning could not be enabled on the bucket {bucket_name} for an unknown reason."
+
+            logger.critical(msg)
+            delete_bucket(client, bucket_name)
+            raise ServerBaseError(msg)
---
+++ backend/tests/unit_tests/utils/test_minio_utils.py
@@ -0,0 +1,55 @@
+from random import randint
+
+import pytest
+from minio import Minio
+from minio.commonconfig import ENABLED
+
+from contaxy.config import Settings, settings
+from contaxy.schema.exceptions import ServerBaseError
+from contaxy.utils.minio_utils import (
+    create_bucket,
+    create_minio_client,
+    get_bucket_name,
+)
+
+
+# TODO:
+@pytest.fixture(scope="session")
+def minio_client() -> Minio:
+    return create_minio_client(settings)
+
+
+@pytest.mark.integration
+class TestMinioUtils:
+    def test_create_client(self) -> None:
+        client = create_minio_client(settings)
+        assert client
+        try:
+            invalid_settings = Settings()
+            invalid_settings.S3_ENDPOINT = "invalid"
+            create_minio_client(invalid_settings)
+            assert False
+        except ServerBaseError:
+            pass
+
+        try:
+            invalid_settings = Settings()
+            invalid_settings.S3_SECRET_KEY = "invalid"
+            create_minio_client(invalid_settings)
+            assert False
+        except ServerBaseError:
+            pass
+
+    def test_create_bucket(self, minio_client: Minio) -> None:
+        project_id = f"{randint(1,10000)}-minio-utils-test"
+        bucket_name = get_bucket_name(project_id, settings.SYSTEM_NAMESPACE)
+        create_bucket(minio_client, bucket_name)
+        assert minio_client.bucket_exists(bucket_name)
+        config = minio_client.get_bucket_versioning(bucket_name)
+        assert config.status == ENABLED
+
+        try:
+            create_bucket(minio_client, bucket_name)
+            assert False
+        except ServerBaseError:
+            pass
