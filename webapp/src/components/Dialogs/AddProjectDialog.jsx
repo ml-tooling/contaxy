b994ce225fc1a88b24695fc55f3da1db51729183
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2021-04-22 11:46:13 +0200

    Use suggest-project-id endpoint in create project

--- webapp/src/components/Dialogs/AddProjectDialog.jsx
+++ webapp/src/components/Dialogs/AddProjectDialog.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -10,10 +11,13 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
 
-const VALID_PROJECT_ID = new RegExp('[^a-z0-9-:/.]');
+import { projectsApi } from '../../services/contaxy-api';
+import showStandardSnackbar from '../../app/showStandardSnackbar';
 
+const VALID_PROJECT_ID = new RegExp('^[a-z0-9-:/.]{0,25}$');
+const VALID_PROJECT_NAME = new RegExp('^[a-zA-Z0-9-s]*$');
 function AddProjectDialog(props) {
-  const { onAdd, onClose } = props;
+  const { className, onAdd, onClose } = props;
   const { t } = useTranslation();
 
   const [projectInput, setProjectInput] = useState({
@@ -21,11 +25,38 @@ function AddProjectDialog(props) {
     name: '',
     description: '',
   });
-  const changeInput = (e) => {
-    setProjectInput({ ...projectInput, [e.target.name]: e.target.value });
+  const [isProjectIdDisabled, setIsProjectIdDisabled] = useState(true);
+
+  const changeInput = async (e) => {
+    const targetName = e.target.name;
+    const targetValue = e.target.value;
+    setProjectInput({ ...projectInput, [targetName]: targetValue });
   };
 
-  const isProjectIdInvalid = VALID_PROJECT_ID.test(projectInput.id);
+  const changeName = async (e) => {
+    const input = e.target.value;
+    let projectId = projectInput.id;
+    if (input && input.length > 3) {
+      try {
+        projectId = await projectsApi.suggestProjectId(input);
+      } catch (err) {
+        showStandardSnackbar(
+          'Error in getting a suggested project id. Please edit it manually.'
+        );
+      }
+    }
+
+    setProjectInput({ ...projectInput, name: input, id: projectId });
+    setIsProjectIdDisabled(true);
+  };
+
+  const isProjectIdValid = VALID_PROJECT_ID.test(projectInput.id);
+  const isProjectNameValid = VALID_PROJECT_NAME.test(projectInput.name);
+
+  let displayNameHelperText = 'The name must be at least 4 characters long.';
+  if (!isProjectNameValid) {
+    displayNameHelperText = 'The name contains invalid characters.';
+  }
 
   return (
     <Dialog open>
@@ -33,25 +64,40 @@ function AddProjectDialog(props) {
       <DialogContent>
         <TextField
           required
-          label="Project Id"
-          name="id"
-          type="text"
-          value={projectInput.id}
-          onChange={changeInput}
-          error={isProjectIdInvalid}
-          helperText={isProjectIdInvalid ? 'Project Id is not valid' : null}
-          margin="dense"
-          fullWidth
-        />
-        <TextField
           label="Project Displayname"
           name="name"
           type="text"
           value={projectInput.name}
-          onChange={changeInput}
+          onChange={changeName}
+          error={!isProjectNameValid}
+          helperText={displayNameHelperText}
           margin="dense"
           fullWidth
         />
+        <div className={`${className} projectIdFields`}>
+          <TextField
+            required
+            disabled={isProjectIdDisabled}
+            label="Project Id"
+            name="id"
+            type="text"
+            value={projectInput.id}
+            onChange={changeInput}
+            error={!isProjectIdValid}
+            helperText={
+              !isProjectIdValid
+                ? 'The project id must be at most 25 characters long and contain only valid characters.'
+                : 'The project id is auto-generated based on the name. You can also edit it manually.'
+            }
+            margin="dense"
+            fullWidth={!isProjectIdDisabled}
+          />
+          {isProjectIdDisabled ? (
+            <Button onClick={() => setIsProjectIdDisabled(false)}>Edit</Button>
+          ) : (
+            false
+          )}
+        </div>
         <TextField
           label="Project Description"
           name="description"
@@ -67,7 +113,7 @@ function AddProjectDialog(props) {
           CANCEL
         </Button>
         <Button
-          disabled={isProjectIdInvalid || !projectInput.id}
+          disabled={!isProjectIdValid || !projectInput.id}
           onClick={() => onAdd(projectInput, onClose)}
           color="primary"
         >
@@ -79,8 +125,19 @@ function AddProjectDialog(props) {
 }
 
 AddProjectDialog.propTypes = {
+  className: PropTypes.string,
   onAdd: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired,
 };
 
-export default AddProjectDialog;
+AddProjectDialog.defaultProps = {
+  className: '',
+};
+
+const StyledAddProjectDialog = styled(AddProjectDialog)`
+  &.projectIdFields {
+    display: flex;
+  }
+`;
+
+export default StyledAddProjectDialog;
