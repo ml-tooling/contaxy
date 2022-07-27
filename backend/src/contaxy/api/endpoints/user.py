4089def6b81eac89ecddc0014e8547583025e8bc
Naveen Vishnu Kinnal <naveen.vishnu.kinnal@sap.com>
2022-07-27 13:48:24 +0200

    Changed the code indentation

--- backend/src/contaxy/api/endpoints/user.py
+++ backend/src/contaxy/api/endpoints/user.py
@@ -199,12 +199,16 @@ def change_password(
         # Only check if token allows admin access on user object
         component_manager.verify_access(token, f"users/{user_id}", AccessLevel.ADMIN)
 
-    if component_manager.get_auth_manager().get_user(user_id).has_password or component_manager.verify_access(token, "users", AccessLevel.ADMIN):
+    if component_manager.get_auth_manager().get_user(
+        user_id
+    ).has_password or component_manager.verify_access(
+        token, "users", AccessLevel.ADMIN
+    ):
         component_manager.get_auth_manager().change_password(user_id, password)
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     else:
         raise PermissionDeniedError(
-            'Password cannot be changed. Please contact an administrator.'
+            "Password cannot be changed. Please contact an administrator."
         )
 
 
